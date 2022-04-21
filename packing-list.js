79b8f50c8713ce90fff58ec0ba547075a9c8dd56
Holly Gardner <hollylizgardner@gmail.com>
2022-04-21 00:52:23 -0400

    Iterating over arrays with while

--- packing-list.js
+++ packing-list.js
@@ -1,3 +1,6 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+
 console.log("Kitchen stuff to pack:");
 let i = 0;
 while (i < packingList.length) {
